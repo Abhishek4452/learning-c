c86d7156ff8a932eaaabaa65977f393bce04767c
Abhishek pokhriyal <abhi445268@gmail.com>
2026-03-21 23:09:39 +0530

    adding reverse linked-list function

--- reverse_linkedList.c
+++ reverse_linkedList.c
@@ -11,6 +11,8 @@ typedef struct Node{
 node* create_node(int data);
 void insert_element(node** head,int data);
 void print_linkedList(node* head);
+void delete_linkedList(node** head,int key);
+node* reverse_linkedlist(node** head);
 int main(){
     node* head = NULL;
     insert_element(&head,25);
@@ -20,6 +22,10 @@ int main(){
     insert_element(&head,93);
     insert_element(&head,37);
     print_linkedList(head);
+    node* head2 = NULL;
+    head2 = reverse_linkedlist(&head);
+    printf("reversing string \n");
+    print_linkedList(head2);
     return 0;
 }
 // ====================== function defination =======================
@@ -53,3 +59,47 @@ void print_linkedList(node* head){
     printf("NULL \n");
 }
 
+void delete_linkedList(node** head,int key){
+    node* temp_prev = NULL; // need to be null
+    node* temp = *head;
+    if (*head == NULL){  // empty case .
+        return;
+    }
+
+
+    if(temp->data == key){ //head to be delete
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    // search case
+    while((temp != NULL) && (temp->data != key) ){
+        temp_prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL){  // not found case
+        return;
+    }
+
+    // if element is found in between 
+    temp_prev->next = temp->next;
+    free(temp);
+    
+}
+
+node* reverse_linkedlist(node** head){
+    //Reverse the links (pointers) between nodes
+    node *temp_prev = NULL;
+    node *curr = *head;
+    node *next = NULL;
+    while(curr != NULL){
+        next = curr->next;
+        curr->next = temp_prev;
+        temp_prev = curr;
+        curr = next;   
+    }
+    *head = temp_prev;
+    return temp_prev;
+}
