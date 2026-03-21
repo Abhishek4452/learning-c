0c3d041659de107adf07c3629cbf21e8f145f07c
Abhishek pokhriyal <abhi445268@gmail.com>
2026-03-21 18:53:05 +0530

    reverse_linkedList, adding new feature of insert element

---
+++ reverse_linkedList.c
@@ -0,0 +1,55 @@
+/* status - not yet complete
+description -Reverse Linked List** [Easy] - *Why:* The quintessential pointer exercise. If you can't reverse a list, you don't fully understand pointers
+*/
+#include <stdio.h>
+#include <stdlib.h>
+// function declaration
+typedef struct Node{
+    int data;
+    struct Node* next;
+}node;
+node* create_node(int data);
+void insert_element(node** head,int data);
+void print_linkedList(node* head);
+int main(){
+    node* head = NULL;
+    insert_element(&head,25);
+    insert_element(&head,2);
+    insert_element(&head,29);
+    insert_element(&head,40);
+    insert_element(&head,93);
+    insert_element(&head,37);
+    print_linkedList(head);
+    return 0;
+}
+// ====================== function defination =======================
+
+node* create_node(int data){
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->next = NULL;
+    newNode->data = data;
+    return newNode;
+}
+
+void insert_element(node** head,int data){
+    node* newNode = create_node(data);
+    if(*head == NULL){
+        *head = newNode;
+        return;
+    }
+    node* temp_head = *head;
+    while(temp_head->next != NULL){
+        temp_head = temp_head->next;
+    }
+    temp_head->next = newNode;
+}
+
+void print_linkedList(node* head){
+    node* temp_head = head;
+    while(temp_head != NULL){
+        printf("%d ->",temp_head->data);
+        temp_head= temp_head->next;
+    }
+    printf("NULL \n");
+}
+
