cefa5913ae7c5d8ac89bb4e8275305b1818c87df
Abhishek pokhriyal <abhi445268@gmail.com>
2026-03-25 22:58:33 +0530

    adding reverseBit file

--- ReverseBits.c
+++ ReverseBits.c
@@ -1,8 +1,15 @@
-/* status -
+/* status - done
 desc -18. **Reverse Bits** [Easy] - *Why:* Tests your understanding of endianness and bit order, vital for SPI protocols .
 */
 #include <stdio.h>
 #include <stdint.h>
+void printBinary(int a,int length){
+    for(int i = length;i>=0;i--){
+        printf("%u",(a>>i)&1);
+        
+    }
+    printf("\n");
+}
 uint32_t RBit(int num){
     uint32_t num2 = 0;
     for(int i=0;i<32;i++){
@@ -13,6 +20,8 @@ uint32_t RBit(int num){
 }
 int main(){
     int num = 29;
-    printf(" num is : %d \n",RBit(num));
+    int Reverse = RBit(num);
+    printf(" num is : %u \n",Reverse);
+    printBinary(Reverse,32);
     return 0;
 }
