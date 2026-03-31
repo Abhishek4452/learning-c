a777d49125a20d5e4c8be74bcf1ffcdc2f5dcc9d
Abhishek pokhriyal <abhi445268@gmail.com>
2026-03-31 21:27:25 +0530

    solving square of sorted array problem

---
+++ squareOfSortedArray.c
@@ -0,0 +1,33 @@
+/* status - 
+42. **Squares of a Sorted Array** [Easy] - *Why:* Two-pointer technique on sorted data .*/
+
+#include <stdio.h>
+#include <stdint.h>
+int a[] = {1,2,3,4,5,6};
+int square(int num);
+void twiceArray(int* arr,int size);
+void printArray(int* arr,int size);
+
+int main(){
+    
+    printArray(a,6);
+    twiceArray(a,6);
+    printArray(a,6);
+    return 0;
+}
+int square(int num){
+    return num*num;
+}
+void twiceArray(int* arr,int size){
+    for(int i =0; i<= ((size-1)/2);i++){
+        arr[i] = square(arr[i]);
+        arr[size-1-i] = square(arr[size-1-i]);
+    }
+}
+void printArray(int* arr,int size){
+    for(int i=0; i< size;i++){
+        printf("%d ",arr[i]);
+    }
+printf("\n");
+}
+
